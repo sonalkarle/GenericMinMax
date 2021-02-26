87ab05577d7add95309d79545d2f9d0806c05d41
sonal <sonalkarle01@gmail.com>
2021-02-26 20:57:55 +0530

    Find maximum string

--- Generic/FindMaxMin.cs
+++ Generic/FindMaxMin.cs
@@ -1,10 +1,82 @@
-﻿using System;
+﻿using findMaxUsingGenerics;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
-namespace Generic
+namespace FindMaxUsingGenerics
 {
-    class FindMaxMin
+    public class findMaximum
     {
+        /// <summary>
+        /// UC1:Find maximum term from integers
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        /// <returns></returns>
+        public int FindMaximumNumber(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            if (firstNumber.CompareTo(secondNumber) > 0 && firstNumber.CompareTo(thirdNumber) > 0)
+            {
+                return firstNumber;
+            }
+            else if (secondNumber.CompareTo(firstNumber) > 0 && secondNumber.CompareTo(thirdNumber) > 0)
+            {
+                return secondNumber;
+            }
+            else if (thirdNumber.CompareTo(firstNumber) > 0 && thirdNumber.CompareTo(secondNumber) > 0)
+            {
+                return thirdNumber;
+            }
+            throw new FindMaximumException("All Values are Same");
+        }
+        /// <summary>
+        /// UC2:Find maximum term from floats
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        /// <returns></returns>
+        public float FindMaximumfloatNumber(float num1, float num2, float num3)
+        {
+            if (num1.CompareTo(num2) > 0 && num1.CompareTo(num3) > 0)
+            {
+                return num1;
+            }
+            else if (num2.CompareTo(num1) > 0 && num2.CompareTo(num3) > 0)
+            {
+                return num2;
+            }
+            else if (num3.CompareTo(num1) > 0 && num3.CompareTo(num2) > 0)
+            {
+                return num3;
+            }
+            throw new FindMaximumException("All Values are Same");
+        }
+
+        /// <summary>
+        /// UC3 : Find maximum string
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <param name="str3"></param>
+        /// <returns></returns>
+        public string FindMaximumstring(string str1, string str2, string str3)
+        {
+            if (str1.CompareTo(str2) > 0 && str1.CompareTo(str3) > 0)
+            {
+                return str1;
+            }
+            else if (str2.CompareTo(str1) > 0 && str2.CompareTo(str3) > 0)
+            {
+                return str2;
+            }
+            else if (str3.CompareTo(str1) > 0 && str3.CompareTo(str2) > 0)
+            {
+                return str3;
+            }
+            throw new FindMaximumException("All Values are Same");
+        }
+
     }
 }
--- GenericTest/UnitTest1.cs
+++ GenericTest/UnitTest1.cs
@@ -1,18 +1,100 @@
 using NUnit.Framework;
-
-namespace GenericTest
+using FindMaxUsingGenerics;
+namespace FindMaximumTest
 {
     public class Tests
     {
+        findMaximum FindMaximum;
         [SetUp]
         public void Setup()
         {
+            FindMaximum = new findMaximum();
+        }
+        /// <summary>
+        /// UC1:Find the maximum integer 
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        [Test]
+        [TestCase(92, 25, 15)]
+        [TestCase(98, 86, 78)]
+        public void GivenFirst(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            Assert.AreEqual(firstNumber, FindMaximum.FindMaximumNumber(firstNumber, secondNumber, thirdNumber));
+        }
+        [Test]
+        [TestCase(85, 95, 25)]
+        [TestCase(18, 96, 28)]
+        public void GivenSecond(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            Assert.AreEqual(secondNumber, FindMaximum.FindMaximumNumber(firstNumber, secondNumber, thirdNumber));
+        }
+        [Test]
+        [TestCase(85, 25, 95)]
+        [TestCase(18, 16, 98)]
+        public void GivenThird(int firstNumber, int secondNumber, int thirdNumber)
+        {
+            Assert.AreEqual(thirdNumber, FindMaximum.FindMaximumNumber(firstNumber, secondNumber, thirdNumber));
+        }
+
+        /// <summary>
+        /// UC2:Find the maximum float 
+        /// </summary>
+        /// <param name="firstNumber"></param>
+        /// <param name="secondNumber"></param>
+        /// <param name="thirdNumber"></param>
+        [Test]
+        [TestCase(97.13f, 23.15f, 23.15f)]
+        [TestCase(98.22f, 45.12f, 89.52f)]
+
+        public void GivenFirstNumFloatMaximum_ReturnMaximum(float num1, float num2, float num3)
+        {
+            Assert.AreEqual(num1, FindMaximum.FindMaximumfloatNumber(num1, num2, num3));
         }
 
         [Test]
-        public void Test1()
+        [TestCase(35.26f, 98.58f, 12.13f)]
+        [TestCase(23.25f, 98.25f, 35.16f)]
+        public void GivenSecondNumFloatMaximum_ReturnMaximum(float num1, float num2, float num3)
         {
-            Assert.Pass();
+            Assert.AreEqual(num2, FindMaximum.FindMaximumfloatNumber(num1, num2, num3));
         }
+
+        [Test]
+        [TestCase(32.57f, 93.55f, 99.30f)]
+        [TestCase(12.25f, 15.23f, 88.12f)]
+        public void GivenThirdNumFloatMaximum_ReturnMaximum(float num1, float num2, float num3)
+        {
+            Assert.AreEqual(num3, FindMaximum.FindMaximumfloatNumber(num1, num2, num3));
+        }
+        /// <summary>
+        /// UC3: Find maximum string 
+        /// </summary>
+        /// <param name="str1"></param>
+        /// <param name="str2"></param>
+        /// <param name="str3"></param>
+        [Test]
+        [TestCase("Xerox", "Abcd", "Pqrs")]
+        public void GivenFirstStringMaximum_ReturnMaximum(string str1, string str2, string str3)
+        {
+            Assert.AreEqual(str1, FindMaximum.FindMaximumstring(str1, str2, str3));
+        }
+
+        [Test]
+        [TestCase("Abcs", "Xerox", "Pqrs")]
+        public void GivenSecondStringMaximum_ReturnMaximum(string str1, string str2, string str3)
+        {
+            Assert.AreEqual(str2, FindMaximum.FindMaximumstring(str1, str2, str3));
+        }
+
+        [Test]
+        [TestCase("Abcd", "Pqrs", "Xerox")]
+        public void GivenThirdStringMaximum_ReturnMaximum(string str1, string str2, string str3)
+        {
+            Assert.AreEqual(str3, FindMaximum.FindMaximumstring(str1, str2, str3));
+        }
+
+
     }
 }
